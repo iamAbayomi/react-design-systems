174428a9434c13d8af92b04f981f84b8678b99ce
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-09-13 07:06:55 +0100

    feature: added customize button

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/ui-components/Button/Button';
+import CustomizeButton from './components/ui-components/Button/CustomizeButton';
 
 function App() {
   return (
     <div className="App">
       <Button/>
+      <CustomizeButton
+          width="124px"
+          height="38px"
+          color="white"
+          backgroundColor="blue"
+       />
     </div>
   );
 }

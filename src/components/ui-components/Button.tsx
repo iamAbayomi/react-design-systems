78133810f7c97ba03d44d25fc905badc856965ca
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-09-13 04:12:32 +0100

    feature: created button components

--- src/components/ui-components/Button.tsx
+++ src/components/ui-components/Button.tsx
@@ -1,5 +1,10 @@
+// This component aims to determine the reusable component
 function Button (){
-
+    return(
+        <div>
+            <button>Save</button>
+        </div>
+    )
 }
 
 export default Button

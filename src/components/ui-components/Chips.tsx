907d9b8793cc6f72bfd1c4a8b11a814bbaa0c825
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-09-13 08:16:11 +0100

    feaure: added chips to main menu

--- src/components/ui-components/Chips.tsx
+++ src/components/ui-components/Chips.tsx
@@ -1,5 +1,30 @@
-function Chips(){
+import  React from 'react'
+import styled from 'styled-components'
 
+type MyProps ={
+    chipsText: string,
+    backgroundColor: string
 }
 
-export default Chips;
+// let backgroundColor = 'green'
+
+
+const Chips: React.FC<MyProps> = (MyProps) =>{
+    
+        return(
+            <div>
+                <ChipsCard className="chips" style={{ backgroundColor: `${MyProps.backgroundColor}` }}>
+                    <p className="chips-text">{MyProps.chipsText}</p>
+                </ChipsCard>
+            </div>
+        )
+}
+
+
+
+const ChipsCard = styled.div `
+    box-sizing: border-box;
+    color: white;
+`
+
+export default Chips
